beb28c67830cc5c57c2f5381523ed4fbc8048ab8
= <=>
2020-09-08 19:01:15 -0400

    rarity updates

--- migrations/4_deploy_erc20_pack_version.js
+++ migrations/4_deploy_erc20_pack_version.js
@@ -10,11 +10,7 @@ module.exports = async function(deployer, network, accounts) {
   }
 
   let erc20, withdrawAddress, perPackAmount;
-  if (network.indexOf('rinkeby') !== -1) {
-    erc20 = '';
-    withdrawAddress = "0x56d76411919Ab8F86D0972b24a9986943193b306";
-    perPackAmount = web3.utils.toWei("0.01", "ether");
-  } else if (network.indexOf('mainnet') !== -1) {
+  if (network.indexOf('mainnet') !== -1) {
     erc20 = '';
     withdrawAddress = "0x56d76411919Ab8F86D0972b24a9986943193b306";
     perPackAmount = web3.utils.toWei("0.01", "ether");
